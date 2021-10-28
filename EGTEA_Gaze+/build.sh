9e1fc5ff9fc7b8b5bf76aa8381f68b4acec694fa
soroush <shahi.varnus@gmail.com>
2021-10-28 14:46:00 -0500

    bash with npy files

--- EGTEA_Gaze+/build.sh
+++ EGTEA_Gaze+/build.sh
@@ -4,11 +4,23 @@ DESTDIR=/ssd2/datasets1
 
 
 VIDEO_CLIPS=$DESTDIR/video_clips.tar
-if [ -f "$FILE" ]; then
-    echo "$FILE exists." &
+HANDS_OBJS=$DESTDIR/hands_obj_npy.zip
+
+
+if [ -f "$VIDEO_CLIPS" ]; then
+    echo "$VIDEO_CLIPS exists."
 else
-    wget -O $DESTDIR/video_clips.tar https://www.dropbox.com/s/udynz2u62wpdva6/video_clips.tar?dl=1 &
+    wget -O $VIDEO_CLIPS https://www.dropbox.com/s/udynz2u62wpdva6/video_clips.tar?dl=1
 fi
 
-tar -xf $DESTDIR/video_clips.tar &
-python3 main.py --datadir $DESTDIR/cropped_clips --outputdir $DESTDIR/frames
+if [ -f "$HANDS_OBJS" ]; then
+    echo "$HANDS_OBJS exists."
+else
+    wget -O $HANDS_OBJS https://www.dropbox.com/s/iyd3flhjfrqqyr6/hand_plus_object_npy_compressed.zip?dl=1
+fi
+
+tar -xf $VIDEO_CLIPS -C $DESTDIR
+unzip $HANDS_OBJS -d $DESTDIR
+rm $VIDEO_CLIPS
+rm $HANDS_OBJS
+#python3 main.py --datadir $DESTDIR/cropped_clips --outputdir $DESTDIR/frames
