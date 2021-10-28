e306e010c0c46765c53150969b6e5693f2c6eab9
Soroush Shahi <shahi.varnus@gmail.com>
2021-10-28 12:06:32 -0500

    fixed bash

--- EGTEA_Gaze+/build.sh
+++ EGTEA_Gaze+/build.sh
@@ -2,6 +2,13 @@
 
 DESTDIR=/ssd2/datasets1
 
-wget -O $DESTDIR/video_clips.tar https://www.dropbox.com/s/udynz2u62wpdva6/video_clips.tar?dl=1
-tar -xf $DESTDIR/video_clips.tar
+
+VIDEO_CLIPS=$DESTDIR/video_clips.tar
+if [ -f "$FILE" ]; then
+    echo "$FILE exists." &
+else
+    wget -O $DESTDIR/video_clips.tar https://www.dropbox.com/s/udynz2u62wpdva6/video_clips.tar?dl=1 &
+fi
+
+tar -xf $DESTDIR/video_clips.tar &
 python3 main.py --datadir $DESTDIR/cropped_clips --outputdir $DESTDIR/frames
